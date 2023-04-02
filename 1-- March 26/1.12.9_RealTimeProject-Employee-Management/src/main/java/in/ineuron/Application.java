d6006cc191a70fcffd8ec002c199182c32361d4b
Safvan <safvanpmcz@gmail.com>
2023-04-02 13:09:23 +0530

    Converted Realworld EMployee Managemnt APplication to SPringboot

---
+++ 1-- March 26/1.12.9_RealTimeProject-Employee-Management/src/main/java/in/ineuron/Application.java
@@ -0,0 +1,39 @@
+package in.ineuron;
+
+import java.util.Scanner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+
+import in.ineuron.controller.IEmployeeController;
+import in.ineuron.vo.EmployeeVO;
+
+@SpringBootApplication
+public class Application
+{
+
+	public static void main(String[] args)
+	{
+		ConfigurableApplicationContext factory = SpringApplication.run(Application.class, args);
+
+		// getting bean using default bean id
+		IEmployeeController controller = factory.getBean("employeeControllerImpl", IEmployeeController.class);
+		System.out.println(controller);
+
+		Scanner sc = new Scanner(System.in);
+
+		EmployeeVO vo = new EmployeeVO();
+
+		System.out.println("Enter employee name :");
+		vo.setEname(sc.nextLine());
+		System.out.println("Enter employee address :");
+		vo.setEaddress(sc.nextLine());
+		System.out.println("Enter employee age :");
+		vo.setEage(sc.next());
+
+		controller.save(vo);
+
+	}
+
+}
