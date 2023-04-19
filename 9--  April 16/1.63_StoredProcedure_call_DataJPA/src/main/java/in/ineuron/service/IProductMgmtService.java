211a8eacae2f87d54442597845391b503dc9ee47
Safvan <safvanpmcz@gmail.com>
2023-04-19 05:52:26 +0530

    StoredProcedure_call_DataJPA

---
+++ 9--  April 16/1.63_StoredProcedure_call_DataJPA/src/main/java/in/ineuron/service/IProductMgmtService.java
@@ -0,0 +1,10 @@
+package in.ineuron.service;
+
+import java.util.List;
+
+import in.ineuron.bo.Product;
+
+public interface IProductMgmtService
+{
+	public List<Product> fetchProductsByName(String product1, String product2);
+}
