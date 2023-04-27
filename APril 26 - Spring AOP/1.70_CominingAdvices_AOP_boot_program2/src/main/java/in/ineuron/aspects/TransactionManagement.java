3ca190f0621b69bf724e18e3055f38b422c773b6
Safvan <safvanpmcz@gmail.com>
2023-04-27 06:56:25 +0530

    AOP second program added

---
+++ APril 26 - Spring AOP/1.70_CominingAdvices_AOP_boot_program2/src/main/java/in/ineuron/aspects/TransactionManagement.java
@@ -0,0 +1,34 @@
+package in.ineuron.aspects;
+
+import org.aspectj.lang.annotation.After;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.annotation.Pointcut;
+import org.springframework.stereotype.Component;
+
+@Aspect
+@Component
+public class TransactionManagement {
+
+	// Syntax:: AS RT packageName.ClassName.methodName(parameter)
+	@Pointcut("execution(public * in.ineuron.dao.*.saveEmployee())")
+	public void saveOperation() {}
+	
+	// Syntax:: AS RT packageName.ClassName.methodName(parameter)
+	@Pointcut("execution(public * in.ineuron.dao.*.deleteEmployee())")
+	public void deleteOperation() {}
+	
+	@Pointcut("saveOperation() || deleteOperation()")
+	public void operation() {}
+
+	@Before("operation()") // JoinPoint
+	public void beginTransaction() {
+		System.out.println("Transaction begin");
+	}
+
+	@After("operation()") // JoinPoint
+	public void commitTransaction() {
+		System.out.println("Transaction commited...");
+	}
+
+}
