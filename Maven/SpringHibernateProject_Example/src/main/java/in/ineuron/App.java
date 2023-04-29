17ee4fe251f4b761c3b6df33aea2649efcb0dcff
Safvan <safvanpmcz@gmail.com>
2023-04-29 18:37:01 +0530

    no codes-adding jars project for demo

---
+++ Maven/SpringHibernateProject_Example/src/main/java/in/ineuron/App.java
@@ -0,0 +1,20 @@
+package in.ineuron;
+
+
+public class App 
+{
+    public static void main( String[] args )
+    {
+       /*
+        * check  pom.xml
+        * =================
+        * this code is just a demo for using properties tag in hibernate
+        * 
+        * exclusion of a  logging jar to avoid conflicts 
+        * 
+        *  adding a rare oracle jar from third party repo
+        *  
+        *  creating a custom goal for executing java program from  maven
+        */
+    }
+}
