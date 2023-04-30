41eaa3ba5298f07169e72d0fabaaf7626c45bfd9
Safvan <safvanpmcz@gmail.com>
2023-04-30 11:16:19 +0530

    MathProj 1 added

---
+++ Maven/Using codes from one maven project to another/MathProj1/src/main/java/in/ineuron/MathApp.java
@@ -0,0 +1,16 @@
+package in.ineuron;
+
+public class MathApp
+{
+	public int add(int x, int y)
+	{
+		return x + y;
+	}
+
+	public static void main(String[] args)
+	{
+		MathApp m = new MathApp();
+		int result = m.add(10, 20);
+		System.out.println("The sum is :: " + result);
+	}
+}
