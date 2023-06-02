408d36194442ab325429dc36a30c90583ec15cea
Safvan <safvanpmcz@gmail.com>
2023-06-02 22:05:10 +0530

    missing project codes added

---
+++ 1-- March 26/1.11_BootProj01-DependancyInjectionTest_LiveClass/src/main/java/in/ineuron/BootProj01DependancyInjectionTestApplication.java
@@ -0,0 +1,46 @@
+package in.ineuron;
+
+import java.time.LocalDate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.annotation.Bean;
+
+import in.ineuron.target.Target;
+
+@SpringBootApplication
+public class BootProj01DependancyInjectionTestApplication
+{
+
+	public static void main(String[] args)
+	{
+		System.out.println("****Container stared******\n");
+		ApplicationContext appContxt = SpringApplication.run(BootProj01DependancyInjectionTestApplication.class, args);
+
+		System.out.println("impl class of ApplicationContext created by IOC:: " + appContxt.getClass().getName());
+
+		Target target = appContxt.getBean(Target.class);
+		System.out.println(target);
+
+	}
+
+	@Bean
+	public LocalDate createSysDate()
+	{
+		return LocalDate.now();
+	}
+
+	// static block and constructor
+	static
+	{
+		System.out.println("BootP--estApplication.class file loading.......");
+	}
+
+	public BootProj01DependancyInjectionTestApplication()
+	{
+		System.out.println("BootP--estApplication obj creation: zero arg constructor ");
+	}
+
+}
