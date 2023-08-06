baf0a30590e35bd484ac45b6358b2cef5707b9ae
Safvan <safvanpmcz@gmail.com>
2023-08-06 08:44:45 +0530

    @NestedClass - Test class inside another Test class

---
+++ JUnit/24-Nested-TestClasses-demo/src/com/safvan/test/CalculatorTest.java
@@ -0,0 +1,58 @@
+package com.safvan.test;
+
+import static org.junit.Assert.assertNotEquals;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+
+import com.safvan.service.Calculator;
+
+class CalculatorTest {
+	private Calculator cobj;
+
+	@BeforeEach
+	public void setUp() {
+		cobj = new Calculator();
+		System.out.println("CalculatorTest.setUp()");
+	}
+
+	@Nested
+	class CalculatorAdditionTest {
+		@Test
+		void additionValidTest() {
+			assertEquals(4, cobj.addition(2, 2));
+			System.out.println("CalculatorTest.CalculatorAdditionTest.additionValidTest()");
+		}
+
+		@Test
+		void additionInvalidTest() {
+			assertNotEquals(5, cobj.addition(2, 2));
+			System.out.println("CalculatorTest.CalculatorAdditionTest.additionInvalidTest()");
+		}
+	}
+
+	@Nested
+	class CalculatorSubstractionTest {
+		@Test
+		void substractionValidTest() {
+			assertEquals(4, cobj.subtraction(6, 2));
+			System.out.println("CalculatorTest.CalculatorSubstractionTest.substractionValidTest()");
+		}
+
+		@Test
+		void substractionInvalidTest() {
+			assertNotEquals(8, cobj.subtraction(6, 2));
+			System.out.println("CalculatorTest.CalculatorSubstractionTest.substractionInvalidTest()");
+		}
+	}
+
+	@AfterEach
+	public void tearDown() {
+		cobj = new Calculator();
+		System.out.println("CalculatorTest.tearDown()");
+	}
+
+}
