89ec9ae432ec0ff7250649a253695234bf58eb81
Safvan <safvanpmcz@gmail.com>
2023-10-07 10:33:54 +0530

    Spring annotation Config - send and recieve TEXT message - ActiveMQ

---
+++ JMS/ActiveMQ1.13-Send-Recieve-Text-Message-to-from-Queue/src/main/java/com/safvan/MessageConsumerApp.java
@@ -0,0 +1,24 @@
+package com.safvan;
+
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.support.AbstractApplicationContext;
+
+import com.safvan.config.AppConfig;
+import com.safvan.receiver.MessageReceiver;
+
+public class MessageConsumerApp
+{
+
+	public static void main(String[] args)
+	{
+		AbstractApplicationContext context = new AnnotationConfigApplicationContext(
+				AppConfig.class);
+
+		MessageReceiver messageReceiver = (MessageReceiver) context.getBean("messageReceiver");
+		String response = messageReceiver.receiveMessage();
+		System.out.println("Messgae Received = " + response);
+
+		((AbstractApplicationContext) context).close();
+	}
+
+}
